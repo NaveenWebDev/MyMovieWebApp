a4c7e046864be694af66e53c18162f9e71182787
NaveenWebDev <ns222280@gmail.com>
2023-06-22 20:08:25 +0530

    after setup firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjI3prR8873VPwLnFL27Hw58U7jPsbjKk",
+  authDomain: "filmeversaapp.firebaseapp.com",
+  projectId: "filmeversaapp",
+  storageBucket: "filmeversaapp.appspot.com",
+  messagingSenderId: "164309286478",
+  appId: "1:164309286478:web:ba2db7b3718a4fda6c4d1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
